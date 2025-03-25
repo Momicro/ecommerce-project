529f6990dc977fb10a708f2d9ecdd964892a11e4
Momicro <momicroa@gmail.com>
2025-03-25 11:23:24 +0100

    Bugs in events

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -6,10 +6,9 @@ import {
   syncUserDeletion,
 } from "@/config/inngest";
 
-export const runtime = "edge";
+//export const runtime = "edge";
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  streaming: "allow",
   functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
 });
